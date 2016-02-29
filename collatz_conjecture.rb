7f5b39c962ddc70f450d9c35af8b4070fc307265
SovietKetchup <sam.comer+git@gmail.com>
2016-02-29 17:12:22 +0000

    Fixed! a few silly mistakes. Now working as intended

--- collatz_conjecture.rb
+++ collatz_conjecture.rb
@@ -1,18 +1,18 @@
 # Collatz Cycle, if odd => 3n + 1, if even => n/2
 # `-- https://en.wikipedia.org/wiki/Collatz_conjecture
 # SovietKetchup
-# v1.0.0
+# v1.1.0
 
 module CollatzCycle
-  def colatz
+  def collatz
     # Check number is positive
-    raise ArgumentError, "Argument is not a positive Integer" unless self < 0
+    raise ArgumentError, "Argument is not a positive Integer" unless self > 0 
     # Initialize values
-    n = self.dup
+    n = self.to_f
     c = 0
     
     # Steps for odd or even until finished
-    until n == 0
+    until n == 1
       if n % 2 == 0
         # Divide by 2
         n /= 2
@@ -20,6 +20,7 @@ module CollatzCycle
         # Times by 3 and Add 1
         n *= 3; n += 1
       end
+      puts n
       c += 1
     end
   end
